84d1c875c51850d2dd4eabd9973bcb1620ce2b3a
Bhav Sagar <bhavsagar5@gmail.com>
2023-06-11 15:59:25 +0530

    Applied folder structuring to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import XpenseItem from "./components/ExpenseItem";
-
+import Expenses from "./components/Expenses/Expenses";
+import './components/Expenses/Expenses.css';
 function App() {
   const expenses = [
     {
@@ -26,26 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <XpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></XpenseItem>
-      <XpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></XpenseItem>
-      <XpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></XpenseItem>
-      <XpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></XpenseItem>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
